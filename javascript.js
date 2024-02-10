1813da58ea9da5fd29b12fabacf05f900fc97c9c
Graphy Dude <khalilrezaie05@gmail.com>
2024-02-10 11:55:48 -0700

    Added my project

---
+++ javascript.js
@@ -0,0 +1,17 @@
+const aboutBtn = document.querySelector(".aboutBtn")
+
+function alertBtn(value) {
+    aboutBtn.href = "#about"
+}
+
+
+
+
+
+// let value = "done,n"
+
+// console.log(value.includes("d"));
+// console.log(value.indexOf("o"));
+// console.log(value.lastIndexOf(","));
+// console.log(value.replace("n",'o'));
+// console.log(value.endsWith(",n"))
