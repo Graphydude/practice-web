445c6a149917e49edbe2408d522da5042e355275
Graphy Dude <khalilrezaie05@gmail.com>
2024-02-23 03:18:09 -0700

    updated

--- javascript.js
+++ javascript.js
@@ -4,11 +4,44 @@ function alertBtn(value) {
     aboutBtn.href = "#about"
 }
 
-const sidebar = document.querySelector(".bi-list")
+const sidebar = document.querySelector(".back-sidebar")
 
 function sidebarBtn(value) {
-    sidebar.display = "block";
+    sidebar.style.display = "block";
 }
+
+const closesidebar = document.querySelector(".back-sidebar")
+
+function closesidebarBtn(value) {
+    closesidebar.style.display = "none"
+}
+
+const dropdown = document.querySelector(".back-side-drop-down")
+
+function dropdownBtn(value) {
+    dropdown.style.display = "block"
+}
+
+const deepdropdown = document.querySelector(".back-deep-side-drop-down")
+
+function deepdropdownBtn(value) {
+    deepdropdown.style.display = "block"
+}
+
+const numbers = 2;
+let table = 10;
+
+for(let i = 1; i <= table; i++) {
+    let result = i*numbers;
+    console.log(`${table} * ${i} = ${result}`)
+}
+
+// prob
+// const closedropdown = document.querySelector(".back-side-drop-down")
+
+// function closedropdownBtn(value) {
+//     closedropdown.style.display = "none"
+// }
 
 // $(document).ready(function(){
 //     $(`.bi-list`).click(function(){
